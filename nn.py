c4244506e0605d4b2ecdf17efee5cd895f838873
berkin tatlisu <barron9@yandex.com>
2020-06-21 13:10:22 +0300

    Update nn.py

--- nn.py
+++ nn.py
@@ -7,6 +7,15 @@ class simpel():
         rg = Generator(PCG64())
         rg.standard_normal()
         #print(rg.standard_normal((3,1)))
+        v=np.array([[0,0,1],
+                    [1,1,1],
+                    [1,0,1],
+                    [0,1,1]])
+        print(v)
+        print(v.T)
+        print("adjustment for .2 error and np.array([[1,1,1]]) output with derivative fn")
+
+        print(np.dot(v.T , ( .02 * np.array([[.1,1,.1,1]]).T * (1 - np.array([[.1,1,.1,1]]).T) ) ))
         print("start weights")
         self.weights=rg.standard_normal((3,1))
         print(self.weights)
