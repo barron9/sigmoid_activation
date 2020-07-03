a4eee4287f3e898b5411368978a9a8fb1a5ba03d
berkin tatlisu <barron9@yandex.com>
2020-07-03 13:59:35 +0300

    Create keras_sigmoid.py

---
+++ keras_sigmoid.py
@@ -0,0 +1,66 @@
+# multivariate mlp example
+from numpy import array
+from numpy import hstack
+from keras.models import Sequential
+from keras.layers import Dense
+
+# # split a multivariate sequence into samples
+# def split_sequences(sequences, n_steps):
+# 	X, y = list(), list()
+# 	for i in range(len(sequences)):
+# 		# find the end of this pattern
+# 		end_ix = i + n_steps
+# 		# check if we are beyond the dataset
+# 		if end_ix > len(sequences):
+# 			break
+# 		# gather input and output parts of the pattern
+# 		seq_x, seq_y = sequences[i:end_ix, :-1], sequences[end_ix-1, -1]
+# 		X.append(seq_x)
+# 		y.append(seq_y)
+# 	return array(X), array(y)
+
+# # define input sequence
+# in_seq1 = array([10, 20, 30, 40, 50, 60, 70, 80, 90])
+# in_seq2 = array([15, 25, 35, 45, 55, 65, 75, 85, 95])
+# out_seq = array([in_seq1[i]+in_seq2[i] for i in range(len(in_seq1))])
+# # convert to [rows, columns] structure
+# in_seq1 = in_seq1.reshape((len(in_seq1),1)) #reshape 1 x 9 to 9 x 1
+# print(in_seq1)
+# in_seq2 = in_seq2.reshape((len(in_seq2), 1))
+# out_seq = out_seq.reshape((len(out_seq), 1))
+# print(out_seq)
+# # horizontally stack columns
+# dataset = hstack((in_seq1, in_seq2, out_seq))
+# print(dataset)
+# # choose a number of time steps
+# n_steps = 3
+# # convert into input/output
+# X, y = split_sequences(dataset, n_steps)
+# print(X,y)
+# # flatten input
+# n_input = X.shape[1] * X.shape[2] # 3col x 2row ---.. 7 total samples
+# print(n_input)
+# X = X.reshape((X.shape[0], n_input))
+# print(X)
+# define model
+model = Sequential()
+model.add(Dense(100, activation='sigmoid', input_dim=3)) #dim 3x2
+model.add(Dense(1))
+model.compile(optimizer='adam', loss='mse')
+# fit model,
+X=array([[1,0,1],
+                [1,1,1],
+                [1,0,0]])
+print(X)
+y=array([0,0,1])
+print(y)
+model.fit(X, y, epochs=101, verbose=0)
+# demonstrate prediction
+# x_input = array([
+#                 [10, 15], 
+#                 [20, 25],
+#                 [30, 35]
+#                 ])
+# x_input = x_input.reshape((1, n_input))
+yhat = model.predict(array([[1,1,1]]), verbose=0)
+print(yhat)
